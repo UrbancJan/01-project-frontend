bc05a622752a7201e8850bbcfd87dfe67bfe67a5
UrbancJan <jan.urbanc4@gmail.com>
2022-03-22 19:28:36 +0100

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login";
+import Nav from "./components/Nav";
 
 function App() {
-  return <Login />;
+  return (
+    <div>
+      <Nav />
+      <Login />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <nav className="navbar">
+      <div>Quotastic</div>
+      <ul className="nav-list">
+        <li className="nav-list-item">lOGIN</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
